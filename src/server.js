92a5b3dbc25e5e380a05326790956e52f376c8cb
TheusH7X5 <matheush7x5@gmail.com>
2023-06-15 17:53:14 -0300

    subindo alteracao no app.use

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const routes = require("./routes/router");
 require("dotenv").config();
-const jwt = require('jsonwebtoken');
+const cors = require("cors");
+
 
 require("./database");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+  next();
+});
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(3001);
